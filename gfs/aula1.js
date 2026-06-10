fce681bcfea7a69ce5c7ab1c16c417f2c1945305
Guilherme F <88292977+Guiisantoos@users.noreply.github.com>
2026-06-10 15:57:47 +0000

    atualizando js

--- gfs/aula1.js
+++ gfs/aula1.js
@@ -3,16 +3,20 @@ console.log("testestestestestestestesteststestes")
 const data = [
     {
         image: "https://picsum.photos/310/301",
-        description: "Eu sou uma imagem"
+        description: "Eu sou uma imagem",
+            titulo:"sou1"
     }, {
         image: "https://picsum.photos/311/302",
-        description: "Eu sou uma imagem"
+        description: "Eu sou uma imagem",
+        titulo:"sou2"
     }, {
         image: "https://picsum.photos/312/303",
-        description: "Eu sou uma imagem"
+        description: "Eu sou uma imagem",
+        titulo:"sou3"
     }, {
         image: "https://picsum.photos/313/304",
-        description: "Eu sou uma imagem"
+        description: "Eu sou uma imagem",
+        titulo:"sou4"
     }
 ]
 
@@ -25,11 +29,19 @@ function loadPage() {
 <div class="card">
     <img src="${card.image}" alt="imagem aleatória">
     <p>${card.description}</p>
-    <button>Mais</button>
+    <button class ="btn-more" data-titulo=${card.titulo}>Mais</button>
     </div>
 
 `).join("")
 
 container.innerHTML=cardHTML;
+
+container.addEventListener('click', (event)=>{
+
+    console.log("Cliquei")
+    if (event.target.classList.contains("btn-more")){
+        console.log(event.target.dataset.titulo)
+    }
+})
 }
 console.log("Container")
